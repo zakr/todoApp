cdd8fe23a70e700de88692ce887654d7c86cd21b
Zak Rome <zak.rome@yahoo.com>
2014-12-03 11:32:41 -0700

    initial commit

---
+++ public/core.js
@@ -0,0 +1,35 @@
+var zakTodo = angular.module('zakTodo', []);
+
+function mainController($scope, $http){
+	$scope.formData = {};
+	$scope.todos = [];
+	$http.get('/api/todos')
+		.success(function(data){
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(data){
+			console.log('Error: ' + data);
+		})
+	$scope.createTodo = function(){
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data){
+				$scope.formData = {};
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			})
+	};
+	$scope.deleteTodo = function(id){
+		$http.delete('/api/todos/' + id)
+			.success(function(data){
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			})
+	}
+}
